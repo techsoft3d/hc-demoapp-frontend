677d1f73fccc1d676873f6193b3fc3be243d6088
Guido <eric5544@gmail.com>
2023-07-08 06:53:04 -0700

    version bumped, moved version and other globals out of startup file

--- app/Admin.js
+++ app/Admin.js
@@ -2,6 +2,14 @@ var userAccount = "demo@techsoft3d.com";
 var userPassword = "demo";
 var startHUB = "demo";
 
+const serveraddress = "https://caas.techsoft3d.com:443";
+const serveraddressBackup = "https://caas2.techsoft3d.com:443";
+
+
+var version = "v0.7.3";
+
+
+
 class Admin {
 
     constructor() {
