ad975dd2109140dbaad18fc465433166469a6348
Ricky Johnson <ricky.johnson@techsoft3d.com>
2024-12-16 16:03:32 -0500

    upgrade to 2024.8.0

--- app/startup.js
+++ app/startup.js
@@ -244,7 +244,7 @@ async function initializeViewer() {
   
   hwv = await myUserManagmentClient.initializeWebviewer("content",data);
   
-  let html = '<span style="width:105px;top:-12px;position:absolute;right:0px">' + version + " / HC 2024.4.0</span>";
+  let html = '<span style="width:105px;top:-12px;position:absolute;right:0px">' + version + " / HC 2024.8.0</span>";
   html += (myAdmin.streamingDisabled || !myUserManagmentClient.getUseStreaming() ? "SCS (serverless)        " : 'Streaming from ' + myUserManagmentClient.getStreamingServerURL());
   $(".versionstring").html(html);
   let screenConfiguration =
